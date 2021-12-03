fe118eaea64e90a1eb16801c889c1c5269fd1606
pfistfl <pfistererf@googlemail.com>
2021-12-03 18:11:20 +0100

    update rbv2

--- hpobench/container/benchmarks/ml/rbv2.py
+++ hpobench/container/benchmarks/ml/rbv2.py
@@ -4,15 +4,17 @@
 """ Benchmark for the rbv2 surrogates Benchmark from hpobench/benchmarks/surrogates/yahpo_gym.py
 Test with
 
-from hpobench.container.benchmarks.surrogates.yahpo_gym import YAHPOGymBenchmark
-b = rbv2Benchmark(container_source=".", container_name="yahpo_gym", scenario = "lcbench", instance = "3945")
+from hpobench.config import HPOBenchConfig
+container_source = HPOBenchConfig().container_dir
+from hpobench.container.benchmarks.ml.rbv2 import rbv2Benchmark
+b = rbv2Benchmark(container_source=".", container_name="rbv2", scenario = "rbv2_rpart", instance = "3945")
 res = b.objective_function(configuration=b.get_configuration_space(seed=1).sample_configuration())
 """
 
 from hpobench.container.client_abstract_benchmark import AbstractBenchmarkClient
 
 
-class YAHPOGymBenchmark(AbstractBenchmarkClient):
+class rbv2Benchmark(AbstractBenchmarkClient):
     def __init__(self, **kwargs):
         kwargs['benchmark_name'] = kwargs.get('benchmark_name', 'rbv2Benchmark')
         kwargs['container_name'] = kwargs.get('container_name', 'rbv2')
